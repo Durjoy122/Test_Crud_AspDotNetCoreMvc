69b4459b8cebf0fb844b3f407060f80ac1f5ce83
Durjoy Sarker <durjoysarker520@gmail.com>
2025-01-19 00:04:11 +0600

    first commit

---
+++ TestProject/TestProject/Controllers/CategoryController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestProject.Data;
+using TestProject.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TestProject.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> obj = _db.Myproperties.ToList();
+            return View(obj);
+        }
+
+        public IActionResult Create()
+        {
+            return View(); //This will return the view page where we can create a new category
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Myproperties.Add(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
---
+++ TestProject/TestProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using TestProject.Models;
+using Microsoft.EntityFrameworkCore;
+namespace TestProject.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+        public DbSet<Category> Myproperties { set; get; } // [Myproperties] is the name of the table in the database
+    }
+}
